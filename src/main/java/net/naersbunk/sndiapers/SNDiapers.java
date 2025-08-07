0b6b9fef62bddc5282035dd5dea105a1d03fb83e
Nathan Nicholson <nnicholson525@gmail.com>
2025-08-06 22:17:35 -0500

    Initial commit

---
+++ src/main/java/net/naersbunk/sndiapers/SNDiapers.java
@@ -0,0 +1,17 @@
+package net.naersbunk.sndiapers;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SNDiapers implements ModInitializer {
+	public static final String MOD_ID = "sndiapers";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/naersbunk/sndiapers/SNDiapersClient.java
@@ -0,0 +1,10 @@
+package net.naersbunk.sndiapers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SNDiapersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
